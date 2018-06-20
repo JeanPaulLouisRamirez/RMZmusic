269029228f0190c418590315794c1604ac088481
Jean Ramirez <jeanramirez@hotmail.fr>
2018-06-20 14:19:54 +0200

    V3

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 const express = require('express');
 const app = express();
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(express.static('RMZ'));
+app.listen(3021, ()=>console.log('example app listening on port 3021'));
